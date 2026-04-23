55f4ed887fc2045ba894325335584aa204284e81
zDefcon <antholozadarequelme@gmail.com>
2026-04-23 16:44:06 -0500

    Agrega clase Persona para gestionar datos personales
    Se crea la clase Persona que incluye atributos como nombre, edad y cédula.
    Se implementan métodos para obtener estos datos y una función para mostrar
    la información personal de manera formateada. Se incluye validación para
    asegurar que la edad no sea negativa.

---
+++ src/E2CarritoCompras/Persona.java
@@ -0,0 +1,38 @@
+package E2CarritoCompras;
+
+
+class Persona {
+    private String nombre;
+    private int edad;
+    private String cedula;
+
+    Persona(String nombre, int edad, String cedula){
+        this.nombre = nombre;
+
+        if(edad < 0){
+            throw new IllegalArgumentException("Error: Edad Negativa");
+        }else{
+            this.edad = edad;
+        }
+
+        this.cedula = cedula;
+    }
+
+    String getNombre(){
+        return nombre;
+    }
+
+    int getEdad(){
+        return edad;
+    }
+
+    String getCedula(){
+        return cedula;
+    }
+
+    String datosPersonales(){
+        return "Nombre: " + getNombre() + "\n" +
+                "Edad: " + getEdad() + "\n" +
+                "Cedula: " + getCedula() + "\n";
+    }
+}
