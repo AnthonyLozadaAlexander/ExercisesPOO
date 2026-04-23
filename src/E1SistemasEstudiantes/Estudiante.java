74661e1dfa56b32451d3a0a0f0a3044534a75b01
zDefcon <antholozadarequelme@gmail.com>
2026-04-22 23:09:31 -0500

    Agrega clase Estudiante con atributos y métodos básicos

---
+++ src/E1SistemasEstudiantes/Estudiante.java
@@ -0,0 +1,29 @@
+﻿package E1SistemasEstudiantes;
+
+class Estudiante {
+
+    private String nombre;
+    private String matricula;
+    private Materia[] materias;
+    private int cantMaterias;
+
+    Estudiante(String nombre, String matricula){
+    this.nombre = nombre;
+    this.matricula = matricula;
+    materias = new Materia[3];
+    cantMaterias = 0;
+    }
+
+    String getNombre(){
+        return nombre;
+    }
+
+    String getMatricula(){
+        return matricula;
+    }
+
+    void agregarMateria(Materia nuevaMateria){
+        materias[cantMaterias] = nuevaMateria;
+        cantMaterias = cantMaterias + 1;
+    }
+}
