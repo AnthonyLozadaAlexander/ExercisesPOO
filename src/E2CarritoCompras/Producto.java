1e00524135118c4686c5d4a531eaab3449e34000
zDefcon <antholozadarequelme@gmail.com>
2026-04-23 00:26:05 -0500

    Agrega clase Producto con atributos y métodos básicos
    Se crea la clase Producto en el paquete E2CarritoCompras,
    que incluye atributos para id, nombre y precio, así como
    métodos para obtener estos valores y mostrar detalles del
    producto. Esto establece la base para la gestión de productos
    en el carrito de compras.

---
+++ src/E2CarritoCompras/Producto.java
@@ -0,0 +1,32 @@
+﻿package E2CarritoCompras;
+
+class Producto {
+
+    private Integer id;
+    private String nombre;
+    private Double precio;
+
+    Producto(Integer id, String nombre, Double precio){
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    Integer getId(){
+        return id;
+    }
+
+    String getNombre(){
+        return nombre;
+    }
+
+    Double getPrecio(){
+        return precio;
+    }
+
+    String mostrarDetalle(){
+        return "[ID: "+getId()+"] \n" +
+                "Nombre Producto: " + getNombre() + "\n" +
+                "$Precio: " + getPrecio() + "\n";
+    }
+}
