05b46e622d32bbf2aaeccf311f4c7f76ba9041f8
zDefcon <antholozadarequelme@gmail.com>
2026-04-23 00:04:02 -0500

    Agrega calificaciones a las materias del estudiante Se establecen calificaciones para las materias Matematicas, Sociales y Quimica en la clase Main. Esto permite una mejor visualización de la información del estudiante al mostrar sus calificaciones.

--- src/E1SistemasEstudiantes/Main.java
+++ src/E1SistemasEstudiantes/Main.java
@@ -4,13 +4,20 @@ public class Main {
     public static void main(String[] args) {
 
         Estudiante E1 = new Estudiante("Karlos", "1234");
+
         Materia Matematicas = new Materia("Matematicas", 6.7);
         Materia Sociales = new Materia("Sociales", 7.4);
         Materia Quimica = new Materia("Quimica", 3.4);
 
         E1.agregarMateria(Matematicas);
+        Matematicas.setCalificacion(3.2);
+
         E1.agregarMateria(Sociales);
+        Sociales.setCalificacion(8.5);
+
         E1.agregarMateria(Quimica);
+        Quimica.setCalificacion(9);
+
         // E1.agregarMateria(new Materia("Ingles", 8.2)); si se agrega una materia mas del arreglo[3] de materias, mandara un error
 
         System.out.println(E1.mostrarInformacion());
