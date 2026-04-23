e5aa791ef65f2fbfb550e18d7e8996b02942b732
zDefcon <antholozadarequelme@gmail.com>
2026-04-22 23:02:19 -0500

    Agrega clase Materia con atributos y métodos básicos
    Se crea la clase Materia que incluye atributos para el nombre y la calificación, junto con métodos para obtener y establecer la calificación, así como para verificar si la materia está aprobada. Esta implementación proporciona una base para manejar las materias en el sistema de estudiantes.

---
+++ src/E1SistemasEstudiantes/Materia.java
@@ -0,0 +1,34 @@
+﻿package E1SistemasEstudiantes;
+
+class Materia {
+
+    private String nombre;
+    private double calificacion;
+
+    Materia(String nombre, double calificacion){
+        this.nombre = nombre;
+        this.calificacion = calificacion;
+    }
+
+    String getNombre(){
+        return nombre;
+    }
+
+    Double getCalificacion(){
+        return calificacion;
+    }
+
+    void setCalificacion(double calificacion){
+        if(getCalificacion() >= 0 && getCalificacion() <= 10.0)
+            this.calificacion = calificacion;
+        else
+            System.out.println("Error: La Calificacion No Esta Entre 0 y 10");
+    }
+
+    Boolean estaAprobado(){
+        if(getCalificacion() >= 6)
+            return true;
+
+        return false;
+    }
+}
