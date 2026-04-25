e90b4cb4e83e7560437782b3d8dec2fc09377913
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-24 20:19:49 -0500

    Agrega clase Gerente para gestionar datos de gerentes
    Se crea la clase Gerente que extiende de Empleado, incluyendo un bono y un método para calcular el pago total. Esto permite una mejor gestión de los datos de los gerentes dentro del sistema de empleados.

---
+++ src/E3SistemaEmpleados/Gerente.java
@@ -0,0 +1,15 @@
+﻿package E3SistemaEmpleados;
+
+public class Gerente extends Empleado{
+    private double bono;
+
+    public Gerente(String nombre, Double salarioBase, double Bono){
+        super(nombre, salarioBase); // inicializa el constructor del padre Empleado
+        this.bono = bono;
+    }
+
+    @Override
+    double calcularPago(){
+        return salarioBase + bono;
+    }
+}
