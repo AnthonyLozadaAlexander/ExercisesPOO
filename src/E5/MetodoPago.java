528ba98bd028a00b7dae074f2767514f8da59472
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-28 01:10:15 -0500

    Agrega clase abstracta MetodoPago con funcionalidad básica
    Se introduce la clase MetodoPago que implementa la interfaz Procesable.
    Esta clase contiene atributos para el titular y el número de transacción,
    y un método para imprimir un recibo con los detalles de la transacción.
    Esto establece una base para diferentes métodos de pago en el sistema.

---
+++ src/E5/MetodoPago.java
@@ -0,0 +1,16 @@
+﻿package E5;
+
+public abstract class MetodoPago implements Procesable {
+    private String titular;
+    private int numTransaccion;
+
+    public MetodoPago(String titular){
+        this.titular = titular;
+        this.numTransaccion = (int) (Math.random() * 1000);
+    }
+
+    void imprimirRecibo(double monto){
+        System.out.println("--Numero De Transaccion ["+(numTransaccion+1)+"]--" + "\n" + "Nombre: " + titular + " \nMonto de $" + monto + "\n");
+
+    }
+}
