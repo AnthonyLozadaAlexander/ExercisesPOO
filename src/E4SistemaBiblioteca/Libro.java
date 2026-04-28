ffe79700ebd125454926c2f5bd0cde56a99607cf
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-27 20:06:07 -0500

    Agrega clase Libro para representar libros en el sistema

---
+++ src/E4SistemaBiblioteca/Libro.java
@@ -0,0 +1,13 @@
+﻿package E4SistemaBiblioteca;
+
+public class Libro {
+    private String titulo;
+    private String autor;
+    private boolean disponible;
+
+    public Libro(String titulo, String autor){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponible = true;
+    }
+}
