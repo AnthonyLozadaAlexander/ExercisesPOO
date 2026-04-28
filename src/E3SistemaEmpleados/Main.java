046c67d5013126ffcfe06ad27ddb29b82f9b45e2
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-27 19:54:48 -0500

    Agrega polimorfismo en la clase Main para calcular pagos
    Se modifica el método main para utilizar un arreglo de empleados,
    permitiendo el uso de polimorfismo al calcular los pagos y mostrar
    los datos de cada empleado, gerente y vendedor de manera más
    estructurada.

--- src/E3SistemaEmpleados/Main.java
+++ src/E3SistemaEmpleados/Main.java
@@ -1,21 +1,28 @@
-﻿package E3SistemaEmpleados;
+package E3SistemaEmpleados;
 
 public class Main {
 
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
-
+        Empleado[] e = new Empleado[3];
         Empleado empleado = new Empleado("Hector", 120.0);
         Gerente gerente = new Gerente("Francisco", 400.0, 100.0);
         Vendedor vendedor = new Vendedor("Mario", 140.0, 20);
 
+        e[0] = empleado;
+        e[1] = gerente;
+        e[2] = vendedor;
+
         vendedor.RegistrarVentas();
         vendedor.RegistrarVentas();
         vendedor.RegistrarVentas();
         vendedor.RegistrarVentas();
 
-        vendedor.calcularPago();
-        gerente.calcularPago();
+        for (int i = 0; i < e.length; i++) {
+             // Polimorfismo
+            e[i].calcularPago();
+            e[i].mostrarDatos();
+        }
 
         sb.append("Empleado: " + "\n" + empleado.mostrarDatos() + "\n").append("Gerente: " + "\n" + gerente.mostrarDatos() + "\n").append("Vendedor: " + "\n" + vendedor.mostrarDatos());
 
