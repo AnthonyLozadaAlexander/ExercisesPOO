fe2f28d5f4321c4f78e59e11773d49922cb6c73b
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-27 20:41:03 -0500

    Agrega método main para inicializar lectores
    Se añade el método main en la clase Main, donde se crean instancias de la clase Lector para los usuarios "Pepito" y "Juanito". Esto permite iniciar la aplicación y gestionar préstamos de libros desde el inicio.

--- src/E4SistemaBiblioteca/Main.java
+++ src/E4SistemaBiblioteca/Main.java
@@ -1,4 +1,8 @@
 ﻿package E4SistemaBiblioteca;
 
 public class Main {
+    public static void main(String[] args) {
+        Lector Pepito = new Lector("Pepito");
+        Lector Juanito = new Lector("Juanito");
+    }
 }
