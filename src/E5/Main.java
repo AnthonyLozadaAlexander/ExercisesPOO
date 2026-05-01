7265227cb83bf8a949d310b1bae0cf066e85b274
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-01 00:19:45 -0500

    Agrega clase Main para ejecutar el procesamiento de pagos
    Se implementa la clase Main que inicializa y ejecuta el procesamiento de pagos utilizando las clases TarjetaCredito y Paypal. Esto permite probar la funcionalidad de las clases de pago en un entorno de ejecución.

---
+++ src/E5/Main.java
@@ -0,0 +1,11 @@
+﻿package E5;
+
+public class Main {
+    public static void main(String[] args) {
+        TarjetaCredito credito = new TarjetaCredito("Pepito", "2026", 2354);
+        Paypal paypal = new Paypal("Juanito", "juanito@gmail.com");
+
+        credito.procesarPago(200.41);
+        paypal.procesarPago(120.43);
+    }
+}
