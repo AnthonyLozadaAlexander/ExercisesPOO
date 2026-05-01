19f507799f7a7041e4829b0f34ee3b8f72a37b6d
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-01 00:52:39 -0500

    Agrega menú interactivo para procesamiento de pagos
    Implementa un menú que permite al usuario elegir entre procesar pagos con tarjeta de crédito o PayPal. Se solicita información al usuario y se crean instancias de las clases correspondientes para procesar el pago. También se calcula y muestra el tiempo total de ejecución.

--- src/E5/Main.java
+++ src/E5/Main.java
@@ -1,13 +1,61 @@
 package E5;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        TarjetaCredito credito = new TarjetaCredito("Pepito", "2026", 2354);
-        Paypal paypal = new Paypal("Juanito", "juanito@gmail.com");
+        Scanner input = new Scanner(System.in);
+        boolean salir = false;
+        String opc = "", titular = "", fechaExperiacion = "", correoElectronico = "";
+        int cvv = 0;
+        long Inicio = System.currentTimeMillis();
+        System.out.println("Iniciando...");
+        do {
+            System.out.println("-----------------------------------------------");
+            System.out.println("                 BIENVENIDO                    ");
+            System.out.println("-----------------------------------------------");
+            System.out.println("Elija el Metodo Para Procesar El Pago");
+            System.out.println("1. Tarjeta De Credito");
+            System.out.println("2. Paypal");
+            System.out.println("3. Salir");
+            opc = input.nextLine();
+            switch(opc){
+                case "1":
+                    System.out.print("Ingrese Su Nombre: ");
+                    titular = input.nextLine();
+                    System.out.print("Ingrese La Fecha De Experiacion: ");
+                    fechaExperiacion = input.nextLine();
+                    System.out.print("Ingrese Su Monto: ");
+                    cvv = input.nextInt();
+                    input.nextLine();
 
-        credito.procesarPago(200.41);
-        paypal.procesarPago(120.43);
+                    TarjetaCredito credito = new TarjetaCredito(titular, fechaExperiacion, cvv);
+                    credito.procesarPago(200.41);
+                    break;
+                case "2":
+                    System.out.print("Ingrese Su Nombre: ");
+                    titular = input.nextLine();
+                    System.out.print("Ingrese Su Correo Electronico: ");
+                    correoElectronico = input.nextLine();
 
+                    Paypal paypal = new Paypal(titular, correoElectronico);
+                    paypal.procesarPago(120.43);
+                    break;
+                case "3":
+                    System.out.println("Gracias Por Usar El Sistema");
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Opcion Invalida");
+                    salir = false;
+                    break;
+            }
+        }while(salir == false);
+
+        input.close();
+        long fin = System.currentTimeMillis();
+        long tiempoTotal = fin - Inicio;
+        System.out.println("Tiempo Total: " + tiempoTotal + " ns");
 
     }
 }
