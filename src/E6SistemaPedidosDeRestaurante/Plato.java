4c95a0de6b4415f27d17dfded1ede00a1528d127
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-09 19:44:24 -0500

    Agrega clase Plato para gestión de platos en el sistema
    Se crea la clase Plato que implementa la interfaz Consumible.
    Esta clase incluye atributos para el nombre, precio base y si es vegetariano.
    Además, se implementan métodos para obtener el precio ajustado y la descripción del plato.

---
+++ src/E6SistemaPedidosDeRestaurante/Plato.java
@@ -0,0 +1,28 @@
+﻿package E6SistemaPedidosDeRestaurante;
+
+public class Plato implements Consumible{
+    private String nombre;
+    private Double precioBase;
+    private Boolean esVegetariano;
+
+    public Plato(String nombre, Double precioBase, Boolean esVegetariano){
+        this.nombre = nombre;
+        this.precioBase = precioBase;
+        this.esVegetariano = esVegetariano;
+    }
+
+    @Override
+    public double getPrecio() {
+        if(esVegetariano){
+             return precioBase + 2.0;
+        }else{
+            return precioBase;
+        }
+    }
+
+    @Override
+    public String getDescripcion() {
+        return "\nPlato: " + nombre + "\n" +
+                "Vegetariano: " + esVegetariano + "\n";
+    }
+}
