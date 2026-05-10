d2b23be7060c52f8cd9fe1685b790c4df6f8c0e9
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-10 17:08:45 -0500

    Agrega clase Pedido para gestionar pedidos en el sistema
    Esta clase permite agregar consumibles a un pedido, calcular el total y mostrar un recibo. Facilita la gestión de pedidos en el sistema de restaurante.

---
+++ src/E6SistemaPedidosDeRestaurante/Pedido.java
@@ -0,0 +1,42 @@
+﻿package E6SistemaPedidosDeRestaurante;
+
+public class Pedido {
+    Consumible[] items;
+    private int cantidadItems;
+
+    public Pedido(int cantidadMaxima){
+        this.cantidadItems = 0;
+        items = new Consumible[cantidadMaxima];
+    }
+
+    void agregarItem(Consumible item){
+        if(cantidadItems < items.length){
+        items[cantidadItems] = item;
+        cantidadItems++;
+        }else{
+            System.out.println("\nPedido Completo\n");
+        }
+    }
+
+    double calcularTotal(){
+        double suma = 0;
+        for (int i = 0; i < items.length; i++) {
+            suma = suma + items[i].getPrecio();
+        }
+        return suma;
+    }
+
+    void mostrarRecibo(){
+        System.out.println("/n------Recibo------");
+        for (int i = 0; i < items.length; i++) {
+            if(items[i] != null) {
+                System.out.println(items[i].getDescripcion());
+                System.out.println(items[i].getPrecio());
+            }
+        }
+        System.out.println("Total a Pagar: " + calcularTotal() + " $");
+        System.out.println("-------------------");
+    }
+
+
+}
