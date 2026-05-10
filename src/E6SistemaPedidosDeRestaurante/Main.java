209feb920a0454c70755836b32b6b5c2893414b7
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-10 17:35:34 -0500

    Agrega lógica para gestionar pedidos en la clase Main
    Se implementa la creación de un pedido que incluye un plato y una bebida.
    Se añaden métodos para calcular el total y mostrar el recibo del pedido.

--- src/E6SistemaPedidosDeRestaurante/Main.java
+++ src/E6SistemaPedidosDeRestaurante/Main.java
@@ -1,7 +1,16 @@
-﻿package E6SistemaPedidosDeRestaurante;
+package E6SistemaPedidosDeRestaurante;
 
 public class Main {
     public static void main(String[] args) {
+
+        Plato p1 = new Plato("Arroz Atun", 23.4, false);
+        Bebida b1 = new Bebida("Coca Cola", 1.50, "Mediana");
+
+        Pedido pedido1  = new Pedido(2);
+        pedido1.agregarItem(p1);
+        pedido1.agregarItem(b1);
+        pedido1.calcularTotal();
+        pedido1.mostrarRecibo();
 
     }
 }
