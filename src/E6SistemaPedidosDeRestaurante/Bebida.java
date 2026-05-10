ab243cb3368d31d5b0519a9d29c258d29375db5b
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-10 17:08:16 -0500

    Agrega clase Bebida para gestión de bebidas en el sistema
    Se implementa la clase Bebida que permite definir el nombre, precio base y tipo de bebida.
    Incluye métodos para obtener el precio según el tamaño y una descripción detallada de la bebida.

---
+++ src/E6SistemaPedidosDeRestaurante/Bebida.java
@@ -0,0 +1,41 @@
+﻿package E6SistemaPedidosDeRestaurante;
+
+public class Bebida implements Consumible {
+    private String nombre;
+    private double precioBase;
+    private String tipo;
+
+    public Bebida(String nombre, Double precioBase, String tipo){
+        this.nombre = nombre;
+        this.precioBase = precioBase;
+        this.tipo = tipo;
+    }
+
+
+    @Override
+    public double getPrecio(){
+
+        switch(tipo) {
+            case "pequeña" -> {
+                return precioBase;
+            }
+
+            case "Mediana" -> {
+                return precioBase + 1.5;
+            }
+
+            case "Grande" -> {
+                return precioBase + 3.0;
+            }
+
+        }
+
+        return -1;
+    }
+
+    @Override
+    public String getDescripcion() {
+        return "\nBebida: " + "[" + nombre + "]\n" +
+                "\nTamaño: " + "[ " + tipo + "]\n";
+    }
+}
