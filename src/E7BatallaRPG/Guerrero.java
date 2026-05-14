16ac7fb9b07cb9b181f3be435c8e6be9a17f79e0
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-14 16:42:36 -0500

    Agrega clase Guerrero para definir un personaje atacante
    Se implementa la clase Guerrero que extiende de Personaje,
    incluyendo un atributo de fuerza extra y un método de ataque
    que calcula el daño total al objetivo. Esto permite una
    mejor gestión de los personajes en el juego.

---
+++ src/E7BatallaRPG/Guerrero.java
@@ -0,0 +1,17 @@
+﻿package E7BatallaRPG;
+
+public class Guerrero extends Personaje{
+    private int fuerzaExtra;
+
+    public Guerrero(String nombre, int vida, int puntosAtaque, int fuerzaExtra){
+    super(nombre, vida, puntosAtaque);
+    this.fuerzaExtra = fuerzaExtra;
+    }
+
+    @Override
+    public void atacar(Personaje Objetivo){
+        int total = (puntosAtaque + fuerzaExtra);
+        Objetivo.recibirDano(total);
+        System.out.println("["+nombre+"] ataca con su espada a [" + Objetivo.getNombre() + "] haciendo ["+ total +"] de danio!");
+    }
+}
