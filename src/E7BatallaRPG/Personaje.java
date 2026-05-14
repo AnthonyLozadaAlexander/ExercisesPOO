f1bdebe10c4c5d9e54e8a8d1564feeb71e9a9b88
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-14 16:42:54 -0500

    Agrega clase abstracta Personaje para definir atributos y comportamiento básico
    Se crea la clase Personaje que implementa la interfaz Atacante. Esta clase define los atributos comunes de los personajes, como nombre, vida y puntos de ataque, así como métodos para recibir daño y verificar si el personaje está vivo. Esto establece una base para la creación de personajes en el juego.

---
+++ src/E7BatallaRPG/Personaje.java
@@ -0,0 +1,35 @@
+package E7BatallaRPG;
+
+public abstract class Personaje implements Atacante{
+    protected String nombre;
+    protected int vida;
+    protected int puntosAtaque;
+
+    public Personaje(String nombre, int puntosVida, int puntosAtaque){
+        this.nombre = nombre;
+        this.vida = puntosVida;
+        this.puntosAtaque = puntosAtaque;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public int getVida(){
+        return vida;
+    }
+
+    public void recibirDano(int dano){
+        vida = vida - dano;
+        if(vida <= 0){
+            System.out.println("\n [" + nombre + "] ha sido derrotado!");
+        }
+    }
+
+    public boolean estaVivo(){
+        if(vida >= 0){
+            return true;
+        }
+        return false;
+    }
+}
