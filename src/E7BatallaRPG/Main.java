a0c9ae3907c774bf28dd1b6d4b1b4ffeaf629770
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-14 17:22:39 -0500

    Agrega interacción para iniciar combate entre personajes
    Se implementa un menú que permite al usuario decidir si desea iniciar un combate entre el Guerrero y el Mago. Se muestra la información de ambos personajes antes de comenzar el combate, y se cierra el escáner al finalizar la ejecución.

--- src/E7BatallaRPG/Main.java
+++ src/E7BatallaRPG/Main.java
@@ -1,15 +1,33 @@
 package E7BatallaRPG;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String respuesta = "";
         Guerrero guerrero = new Guerrero("Thorfin", 100, 10, 5);
         Mago mago = new Mago("Gardalf", 5, 15, 3);
 
-        while(guerrero.estaVivo() && mago.estaVivo()){
-            guerrero.atacar(mago);
-            if(mago.estaVivo()){
-                mago.atacar(guerrero);
+        System.out.println("------------------------------------------------");
+        System.out.println("        Informacion De Los Personajes");
+        System.out.println("------------------------------------------------");
+        System.out.println(guerrero.mostrarInfo());
+        System.out.println("------------------------------------------------");
+        System.out.println(mago.mostrarInfo());
+        System.out.println("------------------------------------------------");
+
+        System.out.println(" Desea Iniciar Un Combate Entre Los Personajes? ");
+        respuesta = input.nextLine();
+        if(respuesta.equalsIgnoreCase("si")){
+            while(guerrero.estaVivo() && mago.estaVivo()){
+                guerrero.atacar(mago);
+                if(mago.estaVivo()){
+                    mago.atacar(guerrero);
+                }
             }
+        }else{
+            System.out.println("El Combate Se Cancelo");
         }
 
         if(guerrero.estaVivo() && !(mago.estaVivo())){
@@ -18,6 +36,7 @@ public class Main {
             System.out.println("El Ganador Fue " + mago.getNombre());
         }
 
+        input.close();
 
     }
 }
