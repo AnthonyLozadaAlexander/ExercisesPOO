d466822875e973506e2bf8e946c0265695dd74b8
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-15 00:14:29 -0500

    Agrega clase CuentaBancaria con métodos para depositar y retirar
    Se crea la clase CuentaBancaria que incluye atributos para el titular, número de cuenta y saldo.
    Implementa métodos para depositar y retirar fondos, así como para transferir entre cuentas,
    y un método estático para mostrar el total de cuentas creadas.

---
+++ src/E8SistemaBancario/CuentaBancaria.java
@@ -0,0 +1,51 @@
+﻿package E8SistemaBancario;
+
+public class CuentaBancaria {
+    private static int totalCuentasCreadas = 0; // comparte el mismo atributo para todas las cuentas
+    private String titular;
+    private int numeroCuenta;
+    private double saldo;
+
+    public CuentaBancaria(String titular, double saldoInicial){
+        this.titular = titular;
+        this.saldo = saldoInicial;
+        totalCuentasCreadas++; // Incrementa el contador cada vez que se crea una nueva cuenta
+        this.numeroCuenta = totalCuentasCreadas;
+    }
+
+    public void depositar(double monto){
+        double total = saldo + monto;
+    }
+
+    public void retirar(double monto){
+        double total = 0.0;
+        if(saldo > 0){
+        total = saldo - monto;
+        }else{
+            System.out.println("Fondos Insuficientes");
+        }
+    }
+
+    static void mostrarTotalCuentas(){
+        System.out.println("Se han creado en el banco un total de: "+ totalCuentasCreadas);
+    }
+
+    void transferir(CuentaBancaria destino, double monto){
+        double total = 0.0;
+        if(this.saldo >= monto){
+              total = saldo - monto;
+              destino.depositar(monto);
+        }
+    }
+
+    void transferir(CuentaBancaria destino, double monto, double comision){
+        double total = 0.0;
+        if(this.saldo >= monto){
+            total = (saldo - monto) - comision;
+            destino.depositar(monto);
+        }
+    }
+
+
+
+}
