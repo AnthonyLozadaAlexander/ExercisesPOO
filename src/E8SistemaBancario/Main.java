efef86d5c579086dce1dc95bc78618273f7313c8
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-15 00:18:31 -0500

    Agrega clase Main para inicializar el sistema bancario
    Se crea la clase Main con el método main que permite
    iniciar el sistema bancario, mostrando el total de cuentas
    y creando dos instancias de CuentaBancaria.

---
+++ src/E8SistemaBancario/Main.java
@@ -0,0 +1,9 @@
+﻿package E8SistemaBancario;
+
+public class Main {
+    public static void main(String[] args) {
+        CuentaBancaria.mostrarTotalCuentas();
+        CuentaBancaria c1 = new CuentaBancaria("Alicia", 500.0);
+        CuentaBancaria c2 = new CuentaBancaria("Bob", 100.0);
+    }
+}
