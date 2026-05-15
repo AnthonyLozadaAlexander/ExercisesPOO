09ddcfaa72d13fb7c375f569462473948afc71bb
zDefcon21 <antholozadarequelme@gmail.com>
2026-05-15 01:38:56 -0500

    Agrega interacción para transferencias en Main
    Se implementa la entrada de usuario para ingresar montos a transferir entre cuentas.
    Esto mejora la funcionalidad de la aplicación al permitir transferencias dinámicas
    en lugar de montos fijos, facilitando así la experiencia del usuario.

--- src/E8SistemaBancario/Main.java
+++ src/E8SistemaBancario/Main.java
@@ -1,13 +1,26 @@
 package E8SistemaBancario;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double monto = 0;
         CuentaBancaria.mostrarTotalCuentas();
         CuentaBancaria c1 = new CuentaBancaria("Alicia", 500.0);
         CuentaBancaria c2 = new CuentaBancaria("Bob", 100.0);
-        c1.transferir(c2, 50.0);
+        System.out.println("--------------------------------------------------");
+        System.out.println("                Transferencias                    ");
+        System.out.println("--------------------------------------------------");
+        System.out.print("Ingrese El Monto A Transferir a la Cuenta " + c2.getTitular() + ": ");
+        monto = input.nextDouble();
+        input.nextLine();
+        c1.transferir(c2, monto);
+        System.out.println("-------------------------------------------------");
+        System.out.print("Ingrese El Monto A Transferir a la Cuenta " + c1.getTitular() + ": ");
+        monto = input.nextDouble();
+        input.nextLine();
         c1.transferir(c2,50.0, 5);
-
         System.out.println("------------------------------------------------");
         System.out.println("        Informacion De Las Cuentas");
         System.out.println("------------------------------------------------");
