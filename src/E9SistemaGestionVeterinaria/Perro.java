fe10ec832fa73da930c3cc8746dc43471cd92294
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-10 17:13:10 -0500

    Agrega clase Perro que extiende de Animal
    Se crea la clase Perro con atributos específicos como raza.
    Incluye métodos para hacer sonido y mostrar información del perro,
    heredando atributos de la clase Animal.

---
+++ src/E9SistemaGestionVeterinaria/Perro.java
@@ -0,0 +1,21 @@
+﻿package E9SistemaGestionVeterinaria;
+
+public class Perro extends Animal{
+    private String raza;
+
+    public Perro(String nombre, int edad, String raza) {
+        super(nombre, edad, TipoAnimal.PERRO); // instanciando los atributos heredados nombre/edad/tipo
+        this.raza = raza;
+    }
+
+    @Override
+    public String hacerSonido() {
+        return "!Guau Guau!";
+    }
+
+    @Override
+    public String mostrarInfo(){
+        return super.mostrarInfo() +
+                    "Raza: " + raza;
+    }
+}
