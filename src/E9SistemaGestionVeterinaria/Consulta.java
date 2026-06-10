784e0737e031b35f75c44c3a961745fc3acc154f
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-10 16:57:56 -0500

    Agrega clase Consulta con atributos y métodos para gestión de consultas
    Se crea la clase Consulta que incluye atributos como motivo, diagnóstico y costo.
    Además, se implementan métodos para obtener estos valores y mostrar la información de la consulta.

---
+++ src/E9SistemaGestionVeterinaria/Consulta.java
@@ -0,0 +1,32 @@
+﻿package E9SistemaGestionVeterinaria;
+
+public class Consulta {
+    private String motivo;
+    private String diagnostico;
+    private double costo;
+
+    public Consulta(String motivo, String diagnostico, double costo){
+        this.motivo = motivo;
+        this.diagnostico = diagnostico;
+        this.costo = costo;
+    }
+
+    public String getMotivo(){
+        return motivo;
+    }
+
+    public String getDiagnostico(){
+        return diagnostico;
+    }
+
+    public double getCosto(){
+        return costo;
+    }
+
+    public String mostrarConsulta(){
+        return "Consulta\n"
+                + "Motivo: " + motivo + "\n" +
+                "Diagnostico: " + diagnostico + "\n" +
+                "Costo: " + costo + "\n";
+    }
+}
