87ac0731534aa619c77eb7003ad8e853b7878b86
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-10 16:57:46 -0500

    Agrega clase abstracta Animal con atributos y métodos básicos
    Se crea la clase Animal que incluye atributos como nombre, edad y tipo, así como un historial de consultas. Se implementan métodos para obtener información del animal, agregar consultas y calcular el costo total de las consultas realizadas.

---
+++ src/E9SistemaGestionVeterinaria/Animal.java
@@ -0,0 +1,42 @@
+﻿package E9SistemaGestionVeterinaria;
+
+import java.util.ArrayList;
+
+public abstract class Animal {
+    String nombre;
+    int edad;
+    TipoAnimal tipo;
+    ArrayList<Consulta> historial;
+
+    public Animal(String nombre, int edad, TipoAnimal tipo){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.tipo = tipo;
+        historial = new ArrayList<>();
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public int getEdad(){
+        return edad;
+    }
+
+    public TipoAnimal getTipo(){
+        return tipo;
+    }
+
+    public void agregarConsulta(Consulta c){
+        historial.add(c);
+    }
+
+    public double calcularCostoTotal(){
+        double total = 0.0;
+        for (int i = 0; i < historial.size(); i++) {
+            total = total + historial.get(i).getCosto();
+        }
+
+        return total;
+    }
+}
