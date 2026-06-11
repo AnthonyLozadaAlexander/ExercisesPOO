19132f8c3f34847161af5c4c322bf1fee66272fb
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-10 20:13:12 -0500

    Agrega métodos para atender consultas y mostrar información en Veterinario
    Se implementa el método atender para registrar consultas de animales y se añade el método mostrarInfo para presentar información del veterinario, incluyendo su nombre, especialidad y total de consultas realizadas.

--- src/E9SistemaGestionVeterinaria/Veterinario.java
+++ src/E9SistemaGestionVeterinaria/Veterinario.java
@@ -1,12 +1,12 @@
 ﻿package E9SistemaGestionVeterinaria;
 
-public class Veterinario{
+public class Veterinario {
 
     private String nombre;
     private String especialidad;
     private int totalConsultasRealizadas = 0;
 
-    public Veterinario(String nombre, String especialidad){
+    public Veterinario(String nombre, String especialidad) {
         this.nombre = nombre;
         this.especialidad = especialidad;
     }
@@ -23,5 +23,17 @@ public class Veterinario{
         return totalConsultasRealizadas;
     }
 
+    public void atender(Animal animal, String motivo, String diagnostico, double costo) {
+        Consulta c = new Consulta(motivo, diagnostico, costo);
+        animal.agregarConsulta(c);
+        totalConsultasRealizadas = totalConsultasRealizadas + 1;
+        System.out.println(" Dr. " + getNombre() + " Atendio a " + animal.getNombre() + " por: " + motivo + "\n");
+    }
 
+
+    public String mostrarInfo() {
+        return "Dr. " + getNombre() + " \n" +
+                "Especialidad: " + getEspecialidad() + "\n" +
+                "Total Consultas: " + totalConsultasRealizadas + "\n";
+    }
 }
