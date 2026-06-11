ef06854bed7b72f6eca4a832c13f08b7a2585c16
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-10 23:36:00 -0500

    Agrega clase Auto para definir comportamiento de automóviles

---
+++ src/E10MRU/Auto.java
@@ -0,0 +1,15 @@
+﻿package E10MRU;
+
+public class Auto extends Vehiculo{
+    private int numeroPasajeros;
+
+    public Auto(String nombre, double velocidad, int numeroPasajeros){
+        super(nombre, velocidad, TipoVehiculo.AUTO);
+        this.numeroPasajeros = numeroPasajeros;
+    }
+
+    @Override
+    public String descripcion(){
+        return "Auto["+nombre+"] | [" + numeroPasajeros + "] pasajeros | ["+velocidad+"] km/h";
+    }
+}
