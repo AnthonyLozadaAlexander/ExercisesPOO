e6112cf224053441c4ce1e489f772bd1c86a5583
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-10 23:07:46 -0500

    Agrega interfaz Movible para definir comportamiento de objetos móviles
    Se crea la interfaz Movible que incluye métodos para mover objetos, calcular el tiempo de desplazamiento y proporcionar una descripción del vehículo. Esto establece una base para implementar clases que representen entidades móviles en el sistema.

---
+++ src/E10MRU/Movible.java
@@ -0,0 +1,8 @@
+﻿package E10MRU;
+
+public interface Movible {
+
+    void mover(double tiempo); // mover objeto
+    double calcularTiempo(double distancia); // calcular el tiempo de recorrer esa distancia
+    String descripcion(); // descripcion del vehiculo
+}
