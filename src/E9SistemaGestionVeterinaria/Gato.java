91158aea1e64cbae5abe6993d484040c511f6743
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-10 20:00:43 -0500

    Agrega clase Gato que extiende de Animal
    Implementa la clase Gato con un atributo que indica si es de interiores.
    Incluye métodos para hacer sonido y mostrar información específica del gato.

---
+++ src/E9SistemaGestionVeterinaria/Gato.java
@@ -0,0 +1,21 @@
+﻿package E9SistemaGestionVeterinaria;
+
+public class Gato extends Animal{
+
+    private boolean esDeInteriores; // por si el gato vive dentro de una casa o no, es decir indoor
+
+    public Gato(String nombre, int edad, boolean esDeInteriores){
+        super(nombre, edad, TipoAnimal.GATO);
+        this.esDeInteriores = esDeInteriores;
+    }
+
+    @Override
+    public String hacerSonido(){
+        return "!Miau";
+    }
+
+    @Override
+    public String mostrarInfo(){
+        return super.mostrarInfo() + " Es Indoor? " + esDeInteriores;
+    }
+}
