fd51ed6ccb88a80f36a096b7539b63dcc19a7ee2
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-10 20:07:22 -0500

    Agrega clase Veterinario con atributos y métodos básicos

---
+++ src/E9SistemaGestionVeterinaria/Veterinario.java
@@ -0,0 +1,27 @@
+﻿package E9SistemaGestionVeterinaria;
+
+public class Veterinario{
+
+    private String nombre;
+    private String especialidad;
+    private int totalConsultasRealizadas = 0;
+
+    public Veterinario(String nombre, String especialidad){
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public int getTotalConsultasRealizadas() {
+        return totalConsultasRealizadas;
+    }
+
+
+}
