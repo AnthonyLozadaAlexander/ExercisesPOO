4f3b471a8c7a21867e12967cfc7496c7b2b50dfd
zDefcon21 <antholozadarequelme@gmail.com>
2026-06-10 23:30:09 -0500

    Agrega clase Vehiculo para definir comportamiento de vehículos
    Se implementa la clase abstracta Vehiculo que implementa la interfaz Movible.
    Esta clase incluye atributos como nombre, velocidad, posición y tipo, así como métodos
    para mover el vehículo, calcular tiempo y distancia, y mostrar su estado.

---
+++ src/E10MRU/Vehiculo.java
@@ -0,0 +1,42 @@
+﻿package E10MRU;
+
+public abstract class Vehiculo implements Movible {
+    protected String nombre;
+    protected double velocidad;
+    protected double posicion;
+    protected TipoVehiculo tipo;
+
+    public Vehiculo(String nombre, double velocidad, TipoVehiculo tipo){
+        this.nombre = nombre;
+        this.velocidad = velocidad;
+        this.tipo = tipo;
+    }
+
+    @Override
+    public void mover(double tiempo){
+        posicion = posicion + (velocidad * tiempo);
+    }
+
+    @Override
+    public double calcularTiempo(double distancia){
+        double tiempo = (distancia / velocidad);
+        return tiempo;
+    }
+
+    public double calcularDistancia(double tiempo){
+        double distancia = (velocidad * tiempo);
+        return distancia;
+    }
+
+    public String mostrarEstado(){
+        return  "Nombre: " + nombre + "\n" +
+                "Tipo: "+ tipo + "\n" +
+                "Velocidad: " + velocidad + "\n" +
+                "Posicion: " + posicion + "\n";
+    }
+
+    public abstract String descripcion();
+
+
+
+}
